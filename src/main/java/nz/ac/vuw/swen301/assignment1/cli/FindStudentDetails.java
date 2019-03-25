01a6dc1e776e2fe9ac660efeb716661d67d3e246
hoongkevi <hoongkevi@myvuw.ac.nz>
2019-03-25 14:05:31 +1300

    completed testing and implemented FindStudentDetails

---
+++ src/main/java/nz/ac/vuw/swen301/assignment1/cli/FindStudentDetails.java
@@ -0,0 +1,22 @@
+package nz.ac.vuw.swen301.assignment1.cli;
+
+import nz.ac.vuw.swen301.assignment1.Student;
+import nz.ac.vuw.swen301.assignment1.StudentManager;
+
+public class FindStudentDetails {
+
+    // THE FOLLOWING METHOD MUST IMPLEMENTED
+    /**
+     * Executable: the user will provide a student id as single argument, and if the details are found,
+     * the respective details are printed to the console.
+     * E.g. a user could invoke this by running "java -cp <someclasspath> nz.ac.vuw.swen301.assignment1.cli.FindStudentDetails id42"
+     * @param arg
+     */
+    public static void main (String[] arg) {
+        String id = arg[0];
+        Student student = StudentManager.readStudent(id);
+        if(student != null) {
+            System.out.println(student.getId() + " " + student.getFirstName() + " " + student.getName() + " " + student.getDegree().getId() + " " + student.getDegree().getName());
+        }
+    }
+}
